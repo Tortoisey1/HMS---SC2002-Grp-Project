fdc14b10a9a5bfbee1b307163ce52e13a22cae25
issac <gohissac27@gmail.com>
2024-10-25 22:47:20 +0800

    Added information access for patient - incomplete

---
+++ src/services/InformationAccessServicePatient.java
@@ -0,0 +1,22 @@
+package services;
+
+import entities.Patient;
+
+public class InformationAccessServicePatient {
+
+    public void viewMedicalRecord() {
+        // Patients can view their own medical record, which consists of: -
+        // Patient ID, Name, Date of Birth, Gender
+        // Contact Information (e.g., phone number, email address)
+        // Blood Type
+        // Past Diagnoses and Treatments
+
+    }
+
+    public void updatePersonalInformation() {
+        // ○ Patients can update non-medical personal information such as email address
+        // and
+        // contact number.
+
+    }
+}
