ec8d261d94752acd62caaf9829baa6940347bf6d
Shreyessk <shreyessk@gmail.com>
2024-11-07 16:38:34 +0800

    Update Global.java

--- src/app/Global.java
+++ src/app/Global.java
@@ -1,10 +1,12 @@
 package app;
 import java.util.Scanner;
-import java.util.Timer;
 
 public class Global {
-    private static Scanner GLOBAL_SCANNER = new Scanner(System.in);
-    public static Scanner getScanner() {
-        return GLOBAL_SCANNER;
+    private static Scanner scanner;
+    public static Scanner getScanner(){
+        if (scanner == null){
+            scanner = new Scanner(System.in);
+        }
+        return scanner;
     }
 }
